6fe48fdca50edd8e2917360df3cafb390dcd7e7b
Your Name <beqasurmana2900@gmail.com>
2024-09-09 20:57:44 +0400

    added destr

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const alphabet = ["A", "B", "C", "D", "E"];
+
+const numbers = [1, 2, 3, 4, 5];
+
+// console.log(alphabet[0]);
+
+// console.log(alphabet[1]);
+
+// console.log(alphabet[2]);
+
+// const [first, second] = alphabet;
+
+// console.log(first, second);
+
+const group3 = [...alphabet, ...numbers];
+
+console.log(group3);
